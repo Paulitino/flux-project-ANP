c8807e0ddad2e40a231990641454df0244db87cb
Paul Faucheur <39162485+Paulitino@users.noreply.github.com>
2021-02-01 21:03:57 +0100

    post handles errors

--- backend/api.js
+++ backend/api.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const db = require('./bdd');
 const expressHandlebars = require('express-handlebars');
 
 app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.engine('hbs', expressHandlebars({
     defaultLayout: 'main.hbs',
@@ -51,6 +54,44 @@ app.get('/subscribe', function(request, response) {
     };
 
     response.status(200).render("subscribe.hbs", model);
+});
+
+app.post('/subscribe', function(request, response) {
+    const firstName = request.body.firstName;
+    const lastName = request.body.lastName;
+    const password = request.body.password;
+    const passwordConfirm = request.body.passwordConfirm;
+    const birthday = request.body.birthday;
+    const company = request.body.company;
+    const country = request.body.country;
+
+    if (password != passwordConfirm) {
+        errors = ["Password and confirmation password are not the same"]
+        const model = {
+            errors: errors,
+            firstName: firstName,
+            lastName: lastName,
+            birthday: birthday,
+            company: company,
+            country: country
+        }
+
+        response.render("subscribe.hbs", model)
+    } else if (password == "") {
+        errors = ["username or password invalid"]
+        const model = {
+            errors: errors,
+            firstName: firstName,
+            lastName: lastName,
+            birthday: birthday,
+            company: company,
+            country: country
+        }
+
+        response.render("subscribe.hbs", model)
+    } else {
+
+    }
 });
 
 app.listen(port=1040, hostname='0.0.0.0', () => {
